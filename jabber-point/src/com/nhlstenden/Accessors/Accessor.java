ceabe7daac074ed203783af4e9ca065346d5ff6b
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2025-03-31 16:57:04 +0200

    missing files

---
+++ jabber-point/src/com/nhlstenden/Accessors/Accessor.java
@@ -0,0 +1,22 @@
+package com.nhlstenden.Accessors;
+
+import com.nhlstenden.Presentation;
+
+import java.io.IOException;
+
+
+public abstract class Accessor {
+    public static final String DEMO_NAME = "Demonstration presentation";
+    public static final String DEFAULT_EXTENSION = ".xml";
+
+    public static Accessor getDemoAccessor() {
+        return new DemoPresentation();
+    }
+
+    public Accessor() {
+    }
+
+    abstract public void loadFile(Presentation presentation, String firstName) throws IOException;
+
+    abstract public void saveFile(Presentation presentation, String firstName) throws IOException;
+}
