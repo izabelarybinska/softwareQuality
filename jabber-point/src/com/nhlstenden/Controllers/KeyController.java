740cc4af1a93d28d2194ac63f62a599a8e0c0360
SaraRodrigues1 <sara.rodrigues.cardoso@student.nhlstenden.com>
2025-03-31 20:11:58 +0200

    Fix: Diagrams and merge resolution

---
+++ jabber-point/src/com/nhlstenden/Controllers/KeyController.java
@@ -0,0 +1,36 @@
+package com.nhlstenden.Controllers;
+
+import com.nhlstenden.Presentation;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
+
+public class KeyController extends KeyAdapter {
+    private Presentation presentation;
+
+    public KeyController(Presentation p) {
+        presentation = p;
+    }
+
+    public void keyPressed(KeyEvent keyEvent) {
+        switch(keyEvent.getKeyCode()) {
+            case KeyEvent.VK_PAGE_DOWN:
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_ENTER:
+            case '+':
+                presentation.nextSlide();
+                break;
+            case KeyEvent.VK_PAGE_UP:
+            case KeyEvent.VK_UP:
+            case '-':
+                presentation.prevSlide();
+                break;
+            case 'q':
+            case 'Q':
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
+    }
+}
